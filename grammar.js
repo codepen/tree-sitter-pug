a91bfcb3e8bf7d06d70555f3af8c7b45c39df368
Dee Vazquez <dee.quez11@gmail.com>
2023-05-04 14:01:42 -0700

    save work

--- grammar.js
+++ grammar.js
@@ -1,6 +1,13 @@
 module.exports = grammar({
   name: "pug",
+
+  externals: ($) => [$._tag_name, $._script_tag_name, $._implicit_end_tag],
+
   rules: {
-    source_file: ($) => "hello",
+    fragment: ($) => repeat($._node),
+
+    _node: ($) => choice($.script_tag),
+
+    script_tag: ($) => seq($._script_tag_name),
   },
 });
